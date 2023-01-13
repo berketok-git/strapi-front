173f132ae9754f22e9f1c9025a65f9f3cbdc2e11
Berke <hasgerian@gmail.com>
2023-01-13 22:01:18 +0300

    for first test

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Todo = () => {
-  const [todos, setTodos] = useState([])
-  const [newTodo, setNewTodo] = useState('')
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
   useEffect(() => {
     const fetchTodos = async () => {
-      const res = await axios.get('http://localhost:1337/api/todos')
-      setTodos(res.data.data)
-    }
+      const res = await axios.get("http://mic-d-loadb-1enycy75ctzrh-1557989190.eu-west-3.elb.amazonaws.com/api/todos");
+      setTodos(res.data.data);
+    };
+    console.log(todos);
     fetchTodos()
-    console.log(todos)
-  }, [])
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!newTodo) return;
     const res = await axios
-      .post("http://localhost:1337/api/todos", {data:{
-        task: newTodo
-      }})
+      .post("http://mic-d-loadb-1enycy75ctzrh-1557989190.eu-west-3.elb.amazonaws.com/api/todos", {
+        data: {
+          task: newTodo,
+        },
+      })
       .then(function (response) {
         console.log(response);
       })
       .catch(function (error) {
         console.log(error);
       });
-      window.location.reload(true)
+      window.location.reload();
+
   };
 
   const handleDelete = async (id) => {
-    await axios.delete(`http://localhost:1337/api/todos/${id}`)
-    setTodos(todos.filter((t) => t.id !== id))
-  }
+    await axios.delete(`http://mic-d-loadb-1enycy75ctzrh-1557989190.eu-west-3.elb.amazonaws.com/api/todos/${id}`);
+    setTodos(todos.filter((t) => t.id !== id));
+  };
 
   return (
     <div>
@@ -54,7 +57,7 @@ const Todo = () => {
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
